ab99d2618982530feaf629e2dc2210d50ef76b9e
koliatest <asicslol1@mail.ru>
2016-12-21 16:32:14 +0200

    fix load cart

--- shop-client/api/actions/cart/load.js
+++ shop-client/api/actions/cart/load.js
@@ -1,14 +1,35 @@
 import Order from '../../models/order';
+import Product from '../../models/product';
 
-// TODO: check if products still exist
 export default function load(req) {
   return new Promise((resolve, reject) => {
     if (req.user) {
       Order.findOne({ userId: req.user._id.toString(), status: 'IN_CART' })
-        .then(
-          order => !order || !order.products.length? resolve(null) : resolve({ order: order, total: order.findTotal() }),
-          err => reject(err)
-        );
+        .then(order => {
+          if (!order || !order.products.length) {
+            resolve(null);
+          } else {
+            let promises = order.products.map(itemCart => {
+              return new Promise((res, rej) => {
+                Product.findById(itemCart.productId)
+                  .then(item => {
+                    itemCart.name = item.name;
+                    itemCart.price = item.price;
+                    itemCart.images = item.images;
+                  })
+                  .then(() => res())
+                  .catch(err => reject(err));
+              });
+            });
+
+            return Promise.all(promises)
+              .then(() => {
+                resolve({ order: order, total: order.findTotal() });
+              })
+              .catch(err => reject(err));
+          }
+        })
+        .catch(err => reject(err));
     } else {
       resolve(null);
     }
